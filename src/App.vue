7451293e2d5941e6f34e31dd12910649c3761c5d
EdemBatalow <batalow.e.i23@gmail.com>
2025-03-31 14:36:19 +0300

    Пункт 2_3

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,24 @@
 <template>
   <v-app>
-    <v-navigation-drawer app></v-navigation-drawer>
-
-    <v-app-bar app dark color="primary">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-spacer></v-spacer>
-
-      <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn>
-          <v-icon start>mdi-cake-variant</v-icon> <!-- Здесь используется встроенная иконка Vuetify -->
-          Link One
-        </v-btn>
-
-        <v-btn>
-          <v-icon start>mdi-cupcake</v-icon>  <!-- Можно использовать другие иконки -->
-          Link Two
-        </v-btn>
-
-        <v-btn> 
-          <v-icon start>mdi-cake-layered</v-icon>
-          Link Three
-        </v-btn>
-      </v-toolbar-items>
-    </v-app-bar>
-
-    <v-main>
-    </v-main>
+  <v-navigation-drawer app v-model="drawer"></v-navigation-drawer> 
+  <v-app-bar app dark color="primary">
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-spacer></v-spacer>
+    <v-toolbar-items class="hidden-sm-and-down">
+       <v-btn>
+<v-icon
+start
+icon="mdi-cake-variant"
+></v-icon>
+Link One
+</v-btn>
+       <v-btn>Link Two</v-btn> 
+       <v-btn>Link Three</v-btn>
+    </v-toolbar-items>
+  </v-app-bar>
+  <v-content>
+    <router-view></router-view> 
+  </v-content>
   </v-app>
 </template>
 
@@ -33,7 +26,7 @@
 export default {
   data() {
     return {
-     
+      drawer: false
     }
   }
 }
