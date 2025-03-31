3f2650d53d4b7b4868a7b9ca9ba4a10c4ad9d439
EdemBatalow <batalow.e.i23@gmail.com>
2025-03-31 17:33:53 +0300

    Пункт 6_3

--- src/views/User/OrdersView.vue
+++ src/views/User/OrdersView.vue
@@ -3,12 +3,15 @@
       <v-row justify="center">
         <v-col cols="12" sm="8" lg="6">
           <h1 class="text--secondary mb-3 mt-3">Orders</h1>
-  
           <v-list subheader two-line flat>
             <v-list-item v-for="order in orders" :key="order.id">
               <template v-slot:prepend>
                 <v-list-item-action>
-                  <v-checkbox :input-value="order.done" color="primary"></v-checkbox>
+                  <v-checkbox
+                    :input-value="order.done"
+                    color="primary"
+                    @click="markDone(order)"
+                  ></v-checkbox>
                 </v-list-item-action>
               </template>
   
@@ -22,7 +25,6 @@
               </template>
             </v-list-item>
           </v-list>
-  
         </v-col>
       </v-row>
     </v-container>
@@ -42,6 +44,12 @@
           }
         ]
       };
+    },
+    methods: {
+      markDone(order) {
+        order.done = !order.done;
+        console.log(order.done);
+      }
     }
   };
   </script>
