77738f3197fa5d66fe9c8bda3c5ce3b6ef4d180e
EdemBatalow <batalow.e.i23@gmail.com>
2025-03-31 21:37:18 +0300

    Пункт 10_4

--- src/store/ads.js
+++ src/store/ads.js
@@ -6,38 +6,41 @@ export default {
           desc: "First Desc",
           promo: true,
           src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
-          id: "1"
+          id: "1",
         },
         {
           title: "Second",
           desc: "Second Desc",
           promo: true,
           src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
-          id: "2"
+          id: "2",
         },
         {
           title: "Third",
           desc: "Third Desc",
           promo: true,
           src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
-          id: "3"
+          id: "3",
         },
         {
           title: "Fourth",
           desc: "Fourth Desc",
           promo: true,
           src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
-          id: "4"
-        }
+          id: "4",
+        },
       ]
     },
     getters: {
+      
       ads(state) {
         return state.ads;
       },
+      
       promoAds(state) {
         return state.ads.filter(ad => ad.promo);
       },
+      
       myAds(state) {
         return state.ads;
       }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,16 @@
   <v-container>
     <!-- Карусель объявлений -->
     <v-row justify="center">
-      <v-col cols="12">
+      <v-col cols="12" xs="12">
         <v-carousel>
           <v-carousel-item
-            v-for="ad in ads"
+            v-for="ad in promoAds"
             :key="ad.id"
             :src="ad.src"
             cover
           >
             <div class="ad-link">
-              <v-btn color="error" :to="'/ad/' + ad.id">
+              <v-btn class="error" :to="'/ad/' + ad.id">
                 {{ ad.title }}
               </v-btn>
             </div>
@@ -21,67 +21,44 @@
     </v-row>
 
     <!-- Grid объявлений -->
-    <v-row>
-      <v-col
-        v-for="ad in ads"
-        :key="ad.id"
-        xs="12"
-        sm="6"
-        md="4"
-      >
-        <v-card>
-          <v-img 
-            :src="ad.src" 
-            :lazy-src="ad.src"
-            height="200px"
-            contain
-          ></v-img>
-          <v-card-title>
-            <h3 class="headline mb-1">{{ ad.title }}</h3>
-            <p>{{ ad.desc }}</p>
-          </v-card-title>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn text :to="'/ad/' + ad.id">Open</v-btn>
-            <v-btn color="primary">Buy</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
-    </v-row>
+    <v-container grid-list-lg>
+      <v-row wrap>
+        <v-col
+          v-for="ad in ads"
+          :key="ad.id"
+          :cols="12"
+          :sm="6"
+          :md="4"
+        >
+          <v-card>
+            <v-img :src="ad.src" height="200px"></v-img>
+            <v-card-title>
+              <div>
+                <h3 class="headline mb-0">{{ ad.title }}</h3>
+                <div>{{ ad.desc }}</div>
+              </div>
+            </v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn text :to="'/ad/' + ad.id">Open</v-btn>
+              <v-btn raised color="primary">Buy</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-container>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      ads: [
-        {
-          title: "First",
-          desc: "First Desc",
-          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
-          id: "1"
-        },
-        {
-          title: "Second",
-          desc: "Second Desc",
-          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
-          id: "2"
-        },
-        {
-          title: "Third",
-          desc: "Third Desc",
-          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
-          id: "3"
-        },
-        {
-          title: "Fourth",
-          desc: "Fourth Desc",
-          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
-          id: "4"
-        }
-      ]
-    };
+  computed: {
+    promoAds() {
+      return this.$store.getters.promoAds;
+    },
+    ads() {
+      return this.$store.getters.ads;
+    }
   }
 };
 </script>
@@ -94,6 +71,7 @@ export default {
   background: rgba(0, 0, 0, 0.5);
   transform: translate(-50%, 0);
   padding: 5px 15px;
-  border-radius: 5px;
+  border-top-right-radius: 5px;
+  border-top-left-radius: 5px;
 }
 </style>
