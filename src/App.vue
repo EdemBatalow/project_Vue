012dc83ab800ab84231db6c3ccddb8df9123fe99
EdemBatalow <batalow.e.i23@gmail.com>
2025-03-31 14:30:54 +0300

    Пункт 2_2

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,40 @@
 <template>
-    <v-app>
-  <v-navigation-drawer app></v-navigation-drawer> 
-  <v-app-bar app dark color="primary">
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-       <v-btn  text>Link One</v-btn> 
-       <v-btn  text>Link Two</v-btn> 
-       <v-btn  text>Link Three</v-btn>
-    </v-toolbar-items>
-  </v-app-bar>
-  <v-main>
-    
-  </v-main>
-  </v-app>
+  <v-app>
+    <v-navigation-drawer app></v-navigation-drawer>
 
-  </template>
-  
-  <script>
-  export default {
+    <v-app-bar app dark color="primary">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn>
+          <v-icon start>mdi-cake-variant</v-icon> <!-- Здесь используется встроенная иконка Vuetify -->
+          Link One
+        </v-btn>
+
+        <v-btn>
+          <v-icon start>mdi-cupcake</v-icon>  <!-- Можно использовать другие иконки -->
+          Link Two
+        </v-btn>
+
+        <v-btn> 
+          <v-icon start>mdi-cake-layered</v-icon>
+          Link Three
+        </v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+
+    <v-main>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+     
+    }
   }
-  </script>
-  
+}
+</script>
