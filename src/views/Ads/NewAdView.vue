788ad329405308fb736be84a62520d85be2400cb
EdemBatalow <batalow.e.i23@gmail.com>
2025-03-31 18:05:31 +0300

    Пункт 7_2

--- src/views/Ads/NewAdView.vue
+++ src/views/Ads/NewAdView.vue
@@ -3,8 +3,12 @@
       <v-row>
         <v-col cols="8" offset="2">
           <h1 class="text--secondary mb-3 mt-3">Create Ad</h1>
+        </v-col>
+      </v-row>
   
-          <v-form v-model="valid" ref="form" lazy-validation>
+      <v-row>
+        <v-col cols="8" offset="2">
+          <v-form v-model="valid" ref="form" validation>
             <v-text-field
               name="title"
               label="Ad Title"
@@ -22,49 +26,45 @@
               class="mb-3"
             ></v-textarea>
           </v-form>
+        </v-col>
+      </v-row>
   
-          <!-- Кнопка загрузки изображения -->
-          <v-row>
-            <v-col cols="8">
-              <v-btn class="mt-3" color="warning">
-                Upload
-                <v-icon right dark>mdi-cloud-upload</v-icon>
-              </v-btn>
-            </v-col>
-          </v-row>
+      <v-row>
+        <v-col cols="8" offset="2">
+          <v-btn class="mt-3" color="warning">
+            Upload
+            <v-icon right dark>mdi-cloud-upload</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
   
-          <!-- Превью изображения -->
-          <v-row>
-            <v-col cols="8">
-              <img
-                :src="imagePreview || 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'"
-                height="150"
-                class="mt-3"
-              />
-            </v-col>
-          </v-row>
+      <v-row>
+        <v-col cols="8" offset="2">
+          <img
+            src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+            height="150"
+            class="mt-3"
+          />
+        </v-col>
+      </v-row>
   
-          <!-- Переключатель Promo -->
-          <v-row>
-            <v-col cols="8">
-              <v-switch v-model="promo" label="Ad to Promo?"></v-switch>
-            </v-col>
-          </v-row>
+      <v-row>
+        <v-col cols="8" offset="2">
+          <v-switch v-model="promo" label="Ad to Promo?"></v-switch>
+        </v-col>
+      </v-row>
   
-          <!-- Кнопка создания объявления -->
-          <v-row>
-            <v-col cols="8">
-              <v-spacer></v-spacer>
-              <v-btn
-                color="success"
-                @click="createAd"
-                :loading="loading"
-                :disabled="!valid || loading"
-              >
-                Create Ad
-              </v-btn>
-            </v-col>
-          </v-row>
+      <v-row>
+        <v-col cols="8" offset="2">
+          <v-spacer></v-spacer>
+          <v-btn
+            color="success"
+            @click="createAd"
+            :loading="loading"
+            :disabled="!valid || loading"
+          >
+            Create Ad
+          </v-btn>
         </v-col>
       </v-row>
     </v-container>
@@ -77,25 +77,22 @@
         valid: false,
         title: "",
         description: "",
-        promo: false,
-        imagePreview: null,
-        loading: false,
+        promo: true,
+        loading: false
       };
     },
     methods: {
       createAd() {
-        if (!this.valid) return;
-        this.loading = true;
-        setTimeout(() => {
-          console.log("Ad Created:", {
+        if (this.$refs.form.validate()) {
+          const ad = {
             title: this.title,
-            description: this.description,
-            promo: this.promo,
-          });
-          this.loading = false;
-        }, 2000);
-      },
-    },
+            desc: this.description,
+            promo: this.promo
+          };
+          console.log(ad);
+        }
+      }
+    }
   };
   </script>
   
