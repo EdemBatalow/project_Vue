cfd8d04d7e5a1208663417373c8df503f3f24b75
EdemBatalow <batalow.e.i23@gmail.com>
2025-03-31 16:51:22 +0300

    Пункт 5_1

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,13 @@
     <v-navigation-drawer app v-model="drawer">
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class="title">
-            КИПУ
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            Учебный проект
-          </v-list-item-subtitle>
+          <v-list-item-title class="title">КИПУ</v-list-item-title>
+          <v-list-item-subtitle>Учебный проект</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
       <v-divider></v-divider>
       <v-list dense>
-        <v-list-item
-          v-for="link in links"
-          :key="link.title"
-          :to="link.url"
-        >
+        <v-list-item v-for="link in links" :key="link.title" :to="link.url">
           <template v-slot:prepend>
             <v-icon :icon="link.icon"></v-icon>
           </template>
@@ -30,26 +22,28 @@
     <!-- Верхняя панель -->
     <v-app-bar app dark color="primary">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <!-- Ссылка на главную страницу -->
+      <v-toolbar-title>
+        <v-btn to="/" text>
+          Home
+        </v-btn>
+      </v-toolbar-title>
+
       <v-spacer></v-spacer>
+
+      <!-- Верхнее меню -->
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn
-          v-for="link in links"
-          :key="link.title"
-          :to="link.url"
-          text
-        >
-          <v-icon
-            start
-            :icon="link.icon"
-          ></v-icon>
+        <v-btn v-for="link in links" :key="link.title" :to="link.url" text>
+          <v-icon start :icon="link.icon"></v-icon>
           {{ link.title }}
         </v-btn>
       </v-toolbar-items>
     </v-app-bar>
 
-    <!-- Основной контент с роутером -->
+    <!-- Основной контент -->
     <v-main>
-      <router-view></router-view> 
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
@@ -58,20 +52,15 @@
 export default {
   data() {
     return {
-      drawer: false, 
+      drawer: false,
       links: [
-        { title: "Home", icon: "mdi-home", url: "/" },
-        { title: "Ads List", icon: "mdi-view-list", url: "/list" },
-        { title: "New Ad", icon: "mdi-note-plus-outline", url: "/new" },
         { title: "Login", icon: "mdi-lock", url: "/login" },
-        { title: "Registration", icon: "mdi-account-plus", url: "/registration" },
-        { title: "My Orders", icon: "mdi-cart", url: "/orders" }
+        { title: "Registration", icon: "mdi-face", url: "/registration" },
+        { title: "Orders", icon: "mdi-bookmark-multiple-outline", url: "/orders" },
+        { title: "New ad", icon: "mdi-note-plus-outline", url: "/new" },
+        { title: "My ads", icon: "mdi-view-list-outline", url: "/list" }
       ]
     };
   }
 };
 </script>
-
-<style scoped>
-
-</style>
